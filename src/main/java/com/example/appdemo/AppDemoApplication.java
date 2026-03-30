68e7e9791412d25b1f8096f1f18e84114a0908c1
huangmh <happyboy3001@foxmail.com>
2026-03-30 15:16:12 +0800

    Add log to check which ClassLoader is used

--- src/main/java/com/example/appdemo/AppDemoApplication.java
+++ src/main/java/com/example/appdemo/AppDemoApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AppDemoApplication {
 
     public static void main(String[] args) {
+        /*查看ClassLoder, 命令行执行java -jar AppDemo.jar*/
+        System.out.println("Main class loader: " + AppDemoApplication.class.getClassLoader());
+        System.out.println("TCCL: " + Thread.currentThread().getContextClassLoader());
         SpringApplication.run(AppDemoApplication.class, args);
     }
 
